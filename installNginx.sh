5855684018b86d407d41a6606259d2c456dd738f
ujez <ujezujez@gmail.com>
2022-06-28 16:57:20 +0100

    little update

--- installNginx.sh
+++ installNginx.sh
@@ -5,9 +5,15 @@ until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
     sleep 1
 done
 
-# install nginx
-apt-get update
-apt-get -y install nginx
-
-# make sure nginx is started
-service nginx start
+sudo apt update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt-get install docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+sudo docker pull nginx:latest
+sudo docker run --name mynginx1 -p 80:80 -d nginx
