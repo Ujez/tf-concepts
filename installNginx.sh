24b3726b3c809ca107ffd4c2af957fe3e67bfb8d
ujez <ujezujez@gmail.com>
2022-06-28 17:38:39 +0100

    little update

--- installNginx.sh
+++ installNginx.sh
@@ -10,7 +10,7 @@ sudo apt-get install apt-transport-https ca-certificates curl software-propertie
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt-get install docker-ce
+sudo apt-get -y install docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo groupadd docker
