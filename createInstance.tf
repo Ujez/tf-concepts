d0f063fe7c33104b8564919a4faa9b05f0f8c0c2
ujez <ujezujez@gmail.com>
2022-06-28 17:22:21 +0100

    little update

--- createInstance.tf
+++ createInstance.tf
@@ -1,3 +1,19 @@
+data "aws_availability_zones" "avilable" {}
+
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 resource "aws_key_pair" "levelup_key" {
   key_name   = "levelup_key"
